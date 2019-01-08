3b9e820c0355d656c407232ef197fe894eb459fd
Vince P <ianparulan@gmail.com>
2019-01-08 12:58:28 -0500

    added typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,12 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-styled-components`,
     {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
---
+++ src/utils/typography.js
@@ -0,0 +1,10 @@
+import Typography from 'typography'
+
+const typography = new Typography({
+  baseFontSize: "18px",
+  baseLineHeight: 1.45,
+  headerFontFamily: ['Poppins', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
+  bodyFontFamily: ['Poppins', 'sans-serif']
+})
+
+export default typography
