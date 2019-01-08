47b93f15d0d537b3b41252e71629270a10fe57e9
Vince P <ianparulan@gmail.com>
2019-01-08 12:44:33 -0500

    added theming

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { ThemeProvider } from 'styled-components'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
@@ -19,21 +20,26 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          {children}
+        <ThemeProvider theme={theme}>
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0px 1.0875rem 1.45rem`,
+              paddingTop: 0,
+            }}
+          >
+            {children}
+          </div>
+        </ThemeProvider>
+        {/* Footer placed in seperate ThemeProvider to avoid Rendering an extra DIV in HTML output  */}
+        <ThemeProvider theme={theme}>
           <footer>
             © {new Date().getFullYear()}, Built with
             {` `}
             <a href="https://www.gatsbyjs.org">Gatsby</a>
           </footer>
-        </div>
+        </ThemeProvider>
       </>
     )}
   />
