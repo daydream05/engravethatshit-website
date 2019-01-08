b270a4b0965687265aa0a63a37f6011329e32825
Vince P <ianparulan@gmail.com>
2019-01-08 12:15:38 -0500

    added styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-transformer-remark`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
---
+++ src/styles/theme.js
@@ -0,0 +1,6 @@
+const theme = {
+  colors: {
+    base: '#001839',
+    primary: '#004FA7',
+  }
+}
