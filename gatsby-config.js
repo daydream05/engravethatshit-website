e8d4372c26987b2d11649fbe64f629e603e4bb04
Vince P <ianparulan@gmail.com>
2019-01-09 15:48:10 -0500

    for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     titleTemplate: '',
-    url: `https://engravethatshit.com/`,
+    url: `https://engravethatshit.netlify.com/`,
     siteUrl,
     twitterUsername: `@engravethatshit`,
     image: `/images/gatsby-astronaut.png`,
